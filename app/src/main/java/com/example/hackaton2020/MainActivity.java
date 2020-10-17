3e394bcb69200119e6d93aaf6e9c52c49e5267f5
ComTols <comtols@outlook.de>
2020-10-17 16:01:29 +0200

    -MainActivity mit unnötigen Methoden vervollständigt
    -Farben sind nun verfügbar:
    --primary, primary_light, primary_dark
    --accent, accent_light, accent_dark
    
    -Dark-Theme und Light-Theme aktualisiert

--- app/src/main/java/com/example/hackaton2020/MainActivity.java
+++ app/src/main/java/com/example/hackaton2020/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.hackaton2020;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Beim erstellen der Activity wird diese Methode aufgerufen
+    //Ruft onStart() auf
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	//Nach dem erstellen und bei jedem Start der Activity wird diese Methode aufgerufen
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onStart() {
+        super.onStart();
     }
+
+    //Nach dem Starten und beim fortsetzten der Activity wird diese Methode aufgerufen
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    //Beim pausieren der Methode wird diese Methode aufgerufen (zB. Activity gerät in den Hintergrund)
+    @Override
+    protected void onPause() {
+	    super.onPause();
+    }
+
+    //Beim beenden der Activity wird diese Mathode aufgerufen (zB. beim minimieren)
+    @Override
+	protected void onStop() {
+		super.onStop();
+	}
+
+	//Nach dem beenden und beim zurückkehren in die Activity wird diese Methode aufgerufen
+    //Ruft onStart() auf
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    //Beim endgültigen beenden wird diese MEthode aufgerufen
+    @Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
 }
