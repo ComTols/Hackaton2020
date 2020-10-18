ed385ee910fd4a634b13a17f861f2686e675ff4d
ComTols <comtols@outlook.de>
2020-10-18 19:59:40 +0200

    Fenster zum auswählen der Benachrichtigungsart hinzugefügt

---
+++ app/src/main/java/com/example/hackaton2020/CreateAccount.java
@@ -0,0 +1,160 @@
+package com.example.hackaton2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CreateAccount extends AppCompatActivity {
+
+	//Setzte Checkboxen als Felder
+	private CheckBox letter;
+	private CheckBox mail;
+	private CheckBox push;
+
+	//Setzte Formulare als Felder
+	private ConstraintLayout letterDescription;
+	private ConstraintLayout mailDescription;
+
+	//Setzte Weiter-Button als Feld
+	private Button cont;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		setContentView(R.layout.activity_create_account);
+		super.onCreate(savedInstanceState);
+
+		//Fülle Felder
+		letter = findViewById(R.id.login_messageOption_letter);
+		mail = findViewById(R.id.login_messageOption_mail);
+		push = findViewById(R.id.login_messageOption_push);
+		letterDescription = findViewById(R.id.login_messageOption_letter_detailContainer);
+		mailDescription = findViewById(R.id.login_messageOption_mail_detailContainer);
+		cont = findViewById(R.id.login_continue);
+
+		letter.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				changeLetterDescriptionVisibility();
+			}
+		});
+
+		mail.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				changeMailDescriptionVisibility();
+			}
+		});
+
+		cont.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onContinue();
+			}
+		});
+	}
+
+	protected void changeLetterDescriptionVisibility() {
+		if (letterDescription.getVisibility() == View.GONE) {
+			letterDescription.setVisibility(View.VISIBLE);
+		} else {
+			letterDescription.setVisibility(View.GONE);
+		}
+	}
+
+	protected void changeMailDescriptionVisibility() {
+		if (mailDescription.getVisibility() == View.GONE) {
+			mailDescription.setVisibility(View.VISIBLE);
+		} else {
+			mailDescription.setVisibility(View.GONE);
+		}
+	}
+
+	protected void onContinue() {
+		boolean isValid = true;
+
+		EditText forename = findViewById(R.id.login_messageOption_letter_detail_firstname);
+		EditText name = findViewById(R.id.login_messageOption_letter_detail_name);
+		EditText street = findViewById(R.id.login_messageOption_letter_detail_street);
+		EditText plz = findViewById(R.id.login_messageOption_letter_detail_postcode);
+		EditText city = findViewById(R.id.login_messageOption_letter_detail_city);
+
+		if (letter.isChecked()) {
+			String errorMes = "";
+
+			System.out.println("Vorname " + forename.getText());
+
+			if (forename.getText().toString().matches("")) {
+				errorMes += "Der Vorname ist erforderlich\n";
+			}
+			if (name.getText().toString().matches("")) {
+				errorMes += "Der Nachname ist erforderlich!\n";
+			}
+			if (street.getText().toString().matches("")) {
+				errorMes += "Eine Straße und eine Hausnummer ist erforderlich!\n";
+			}
+			if (plz.getText().toString().matches("")) {
+				errorMes += "Eine Postleitzahl ist erforderlich!\n";
+			}
+			if (city.getText().toString().matches("")) {
+				errorMes += "Eine Ortsangabe ist erforderlich!";
+			}
+
+			TextView errorLetter = findViewById(R.id.login_messageOption_errorLetter);
+			if (!(errorMes.equals(""))) {
+				isValid = false;
+				errorLetter.setText(errorMes);
+				errorLetter.setVisibility(View.VISIBLE);
+			} else {
+				errorLetter.setVisibility(View.GONE);
+			}
+		}
+		EditText mailTxt = findViewById(R.id.login_messageOption_detail_mail);
+		if (mail.isChecked()) {
+			TextView errorMail = findViewById(R.id.login_messageOption_errorMail);
+
+			if (mailTxt.getText().toString().matches("") || !(mailTxt.getText().toString().contains("@"))) {
+				isValid = false;
+				errorMail.setText("Eine E-Mail Adresse ist erforderlich!");
+				errorMail.setVisibility(View.VISIBLE);
+			} else {
+				errorMail.setVisibility(View.GONE);
+			}
+		}
+
+		if(isValid) {
+			SharedPreferences accountDetails = getSharedPreferences("accountDetails", MODE_PRIVATE);
+			SharedPreferences.Editor editor = accountDetails.edit();
+
+			editor.putBoolean("initialized", true);
+			editor.putBoolean("messageLetter", letter.isChecked());
+			editor.putBoolean("messageMail", mail.isChecked());
+			editor.putBoolean("messagePush", push.isChecked());
+			editor.putBoolean("messageInApp", true);
+
+			if(letter.isChecked()) {
+				editor.putString("forename", forename.getText().toString());
+				editor.putString("forename", name.getText().toString());
+				editor.putString("forename", street.getText().toString());
+				editor.putString("forename", plz.getText().toString());
+				editor.putString("forename", city.getText().toString());
+			}
+			if(mail.isChecked()) {
+				editor.putString("mail", mailTxt.getText().toString());
+			}
+
+			editor.apply();
+
+			finish();
+		}
+	}
+}
--- app/src/main/java/com/example/hackaton2020/MainActivity.java
+++ app/src/main/java/com/example/hackaton2020/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.hackaton2020;
 
 import android.Manifest;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -21,70 +23,77 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
 	private ZXingScannerView scannerView;
 	private TextView textResult;
 
-    //Beim erstellen der Activity wird diese Methode aufgerufen
-    //Ruft onStart() auf
+	//Beim erstellen der Activity wird diese Methode aufgerufen
+	//Ruft onStart() auf
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		checkAccountInitialized();
+
 		setContentView(R.layout.activity_main);
-		scannerView = (ZXingScannerView)findViewById(R.id.zxscan);
-		textResult = (TextView) findViewById(R.id.txtBarcodeValue);
+		scannerView = findViewById(R.id.zxscan);
+		textResult = findViewById(R.id.txtBarcodeValue);
+	}
 
-		Dexter.withActivity(this)
-				.withPermission(Manifest.permission.CAMERA)
-				.withListener(new PermissionListener() {
-					@Override
-					public void onPermissionGranted(PermissionGrantedResponse response) {
-						scannerView.setResultHandler(MainActivity.this);
-						scannerView.startCamera();
-					}
-
-					@Override
-					public void onPermissionDenied(PermissionDeniedResponse response) {
-
-					}
-
-					@Override
-					public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
-
-					}
-				})
-				.check();
+	private void checkAccountInitialized() {
+		SharedPreferences accountDetails = getSharedPreferences("accountDetails", MODE_PRIVATE);
+		if (!accountDetails.contains("initialized")) {
+			Intent intentCreateAccount = new Intent(this, CreateAccount.class);
+			startActivity(intentCreateAccount);
+		}
 	}
 
 	//Nach dem erstellen und bei jedem Start der Activity wird diese Methode aufgerufen
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
+	@Override
+	protected void onStart() {
+		Dexter.withActivity(this)
+			.withPermission(Manifest.permission.CAMERA)
+			.withListener(new PermissionListener() {
+				@Override
+				public void onPermissionGranted(PermissionGrantedResponse response) {
+					scannerView.setResultHandler(MainActivity.this);
+					scannerView.startCamera();
+				}
+				@Override
+				public void onPermissionDenied(PermissionDeniedResponse response) {
+				}
+				@Override
+				public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+				}
+			})
+			.check();
+		super.onStart();
+	}
 
-    //Nach dem Starten und beim fortsetzten der Activity wird diese Methode aufgerufen
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
+	//Nach dem Starten und beim fortsetzten der Activity wird diese Methode aufgerufen
+	@Override
+	protected void onResume() {
+		checkAccountInitialized();
+		super.onResume();
+	}
 
-    //Beim pausieren der Methode wird diese Methode aufgerufen (zB. Activity gerät in den Hintergrund)
-    @Override
-    protected void onPause() {
-	    super.onPause();
-    }
+	//Beim pausieren der Methode wird diese Methode aufgerufen (zB. Activity gerät in den Hintergrund)
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
 
-    //Beim beenden der Activity wird diese Mathode aufgerufen (zB. beim minimieren)
-    @Override
+	//Beim beenden der Activity wird diese Mathode aufgerufen (zB. beim minimieren)
+	@Override
 	protected void onStop() {
 		super.onStop();
 	}
 
 	//Nach dem beenden und beim zurückkehren in die Activity wird diese Methode aufgerufen
-    //Ruft onStart() auf
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-    }
+	//Ruft onStart() auf
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+	}
 
-    //Beim endgültigen beenden wird diese Methode aufgerufen
-    @Override
+	//Beim endgültigen beenden wird diese Methode aufgerufen
+	@Override
 	protected void onDestroy() {
 		scannerView.stopCamera();
 		super.onDestroy();
