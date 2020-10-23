d87328d46c3504c3d2ba6fd8773672076d498262
DLemming <david.lemming28@gmail.com>
2020-10-23 20:37:50 +0200

    Sidebar und Dark-Mode - Sidebar funktionsfähig - Voll funktionierender Dark-Mode (Burger muss lediglich noch weiß gemacht werden)
    TODOS:
    - Sidebar mit Inhalt füllen
    - Homescreen muss besuchte Restaurants auslesen
    - Dark-Mode: Burger muss weiß sein

--- app/src/main/java/com/example/hackaton2020/HomeScreen.java
+++ app/src/main/java/com/example/hackaton2020/HomeScreen.java
@@ -1,17 +1,73 @@
 package com.example.hackaton2020;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.net.wifi.hotspot2.pps.HomeSp;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeScreen extends AppCompatActivity {
+
+    //Instanzierung der UI-Elemente
+    private ActionBarDrawerToggle toggle;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        //Deklarierung der UI-Elemente
+        drawerLayout=findViewById(R.id.drawerLayout);
+        navigationView=findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+        //Toolbar wird als Actionbar der Activity gesetzt !!!WICHTIG!!! --> Dark Mode: Burger muss noch white_medium_emphasis sein
+        setSupportActionBar(toolbar);
+        //Titel der Actionbar
+        getSupportActionBar().setTitle("Hackathon");
+
+        //Öffnen/Schließen der Sidebar wird Instanziert und Deklariert
+        toggle=new ActionBarDrawerToggle(this,drawerLayout, R.string.home_screen_open,R.string.home_screen_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.item1:
+                        Toast.makeText(HomeScreen.this, "item1", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.item2:
+                        Toast.makeText(HomeScreen.this, "item2", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.item3:
+                        Toast.makeText(HomeScreen.this, "item3", Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
