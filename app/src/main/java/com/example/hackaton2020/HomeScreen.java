fc18c32ea4a40e8d60b7b77eaf844e9f28bb95ed
DLemming <david.lemming28@gmail.com>
2020-10-24 21:58:25 +0200

    Home screen updating lastly visited restaurants programmatically - done
    TODO:
    - adjust Restaurant-name size
    - format dateTime

--- app/src/main/java/com/example/hackaton2020/HomeScreen.java
+++ app/src/main/java/com/example/hackaton2020/HomeScreen.java
@@ -3,19 +3,34 @@ package com.example.hackaton2020;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ContextThemeWrapper;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.net.wifi.hotspot2.pps.HomeSp;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.LinearLayout.LayoutParams;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+
+
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -24,6 +39,9 @@ public class HomeScreen extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
     private Toolbar toolbar;
+    private LinearLayout linearLayout;
+    private TextView textViewRestaurant;
+    private TextView textViewDateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,9 +69,6 @@ public class HomeScreen extends AppCompatActivity {
                     case R.id.item1:
                         Toast.makeText(HomeScreen.this, "item1", Toast.LENGTH_SHORT).show();
                         return true;
-                    case R.id.item2:
-                        Toast.makeText(HomeScreen.this, "item2", Toast.LENGTH_SHORT).show();
-                        return true;
                     case R.id.item3:
                         Toast.makeText(HomeScreen.this, "item3", Toast.LENGTH_SHORT).show();
                         return true;
@@ -61,6 +76,9 @@ public class HomeScreen extends AppCompatActivity {
                 return true;
             }
         });
+
+        linearLayout = findViewById(R.id.linearLayoutMain);
+        fillRestaurantCards();
     }
 
     @Override
@@ -69,5 +87,43 @@ public class HomeScreen extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void fillRestaurantCards()
+    {
+        SharedPreferences accountDetails = getSharedPreferences("accountDetails", MODE_PRIVATE);
+        String json = accountDetails.getString("savedData", null);
+        Gson gson = new Gson();
+        Type type = new TypeToken<ChargedData>() {}.getType();
+        //System.out.println(json);
+        ChargedData chargedData = gson.fromJson(json, type);
+        ArrayList<ChargedData.Events> events = chargedData.getEvents();
+        System.out.println(events);
+
+        if(events.size() > 0)
+        {
+            linearLayout = findViewById(R.id.linearLayoutMain);
+
+            for(int i = events.size()-1; i >= 0; i--) {
+                drawCardsonHome(events.get(i).name, events.get(i).startTime);
+            }
+        }else{
+            TextView textView = new TextView(this);
+            textView.setText("Keine Veranstaltungen in den letzten 14 Tagen besucht.");
+            linearLayout.addView(textView);
+        }
+    }
+
+    public void drawCardsonHome(String restaurant, String datumUhrzeit)
+    {
+        View cardView = getLayoutInflater().inflate(R.layout.add_home_screen_card, null, false);
+
+        textViewRestaurant = cardView.findViewById(R.id.textViewRestaurant);
+        textViewRestaurant.setText(restaurant);
+
+        textViewDateTime = cardView.findViewById(R.id.textViewDateTime);
+        textViewDateTime.setText(datumUhrzeit);
+
+        linearLayout.addView(cardView);
     }
 }
